fe2a4a694240f546bf9a861a581f8d84a4bedef4
Ando <ando@ut.ee>
2014-02-17 19:37:19 +0200

    Trying .php.

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ 
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame. Remove this if you use the .htaccess -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+ 
+    <title>Website Title</title>
+ 
+    <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+ 
+    <!-- Generate Favicon Using 1.http://tools.dynamicdrive.com/favicon/ OR 2.http://www.favicon.cc/ -->
+    <link rel="shortcut icon" href="images/favicon.png" />
+ 
+</head>
+<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
+
+  <div class="header">
+   <input type="button" value="Logi sisse" onClick="location.href='login.html'">
+   <input type="button" value="Registreeri" onClick="location.href='registration.html'">
+ </div>
---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php include 'header.php';?>
+
+<div class="sidebar">
+      <div class="search-triangle">  
+          <a href="/otsing.html">
+          <span class="search-button">OTSING</span>
+          </a>
+      </div>
+  </div>
+  <a href="/objekt.html">
+  <div class="content">
+      <h1>OBJECT</h1>
+    
+  </div>
+  </a>
+
+</body>
+</html>
