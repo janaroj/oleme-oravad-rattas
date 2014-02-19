a06be8e040368b26dfd063904603e439ec448b2b
Janar Ojalaid <janarojalaid@gmail.com>
2014-02-19 14:43:04 +0200

    fix for button
    fix

--- registration.php
+++ registration.php
@@ -5,11 +5,11 @@
 
   <div class="content">
         <div class="registration-form">
-            <form>
-            Kasutajanimi: <input type="text" name="userName"><br>
-            Parool: <input type="password" name="passWord"><br>
-            Email: <input type="email" name="email"><br>
-            Telefon: <input type="number" name="phone"><br>
+            <form action="">
+            Kasutajanimi: <input type="text" name="userName" /><br/>
+            Parool: <input type="password" name="passWord" /><br/>
+            Email: <input type="email" name="email" /><br/>
+            Telefon: <input type="number" name="phone" /><br/>
             <!--
             <input type="button" value="Tühista" onClick="parent.location='index.html'">
             -->            
