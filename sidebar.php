560d25f637fc7dfb169f3d5da14b5babd6b57c0b
Ando <ando@ut.ee>
2014-02-21 09:13:44 +0200

    Sidebar fix.

--- sidebar.php
+++ sidebar.php
@@ -2,9 +2,9 @@
 <div class="side-nav">  
             <button class="add-car">Lisa auto</button>
             <button class="change-car">Muuda oma autosid</button>                        
-            <button class=<?php if(!$_GET) echo 'car' ?>>Auto 1</button>
-            <button class=<?php if(!$_GET) echo 'car' ?>>Auto 2</button>
-            <button class=<?php if(!$_GET) echo 'car' ?>>Auto 3</button>
+            <button class=<?php if(!$_GET) echo "car" ?>>Auto 1</button>
+            <button class=<?php if(!$_GET) echo "car" ?>>Auto 2</button>
+            <button class=<?php if(!$_GET) echo "car" ?>>Auto 3</button>
             <button class="my-querys">Vaata minule tehtud päringuid</button>
             <button class="change-settings">Muuda oma kasutaja seadeid</button>
             <button class="logout">Logi välja</button>
