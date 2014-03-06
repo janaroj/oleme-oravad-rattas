26f67fa84cba860b4b48426d88c0027daddfcd17
Janar Ojalaid <janarojalaid@gmail.com>
2014-03-07 00:02:40 +0200

    join query

--- protected/views/site/myUser.php
+++ protected/views/site/myUser.php
@@ -17,6 +17,21 @@
 		<div class="content-small">
 		    <h1><?php echo "Tere ";echo Yii::app()->user->getName();?></h1> 
 			<p>Siin on sinu kasutaja leht</p>
+			<p>Sul on 
+			
+			
+		<?php
+		$userId = Yii::app()->user->id;
+		$sql="SELECT count(*) as total from users inner join cars on users.id = cars.userId where users.id = $userId";
+		$connection=Yii::app()->db; 
+		$command=$connection->createCommand($sql);
+		$result=$command->query();
+		foreach ($result as $value) {
+			echo $value['total'];
+		}
+   		
+
+?> autot</p>
 		</div>
 		<div class="clear"></div>
 	</div>
