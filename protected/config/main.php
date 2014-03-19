9201c3ad126ae7fc2686e7720d908a33a07b5159
Ando <ando@ladaklubi.ee>
2014-03-19 19:15:21 +0200

    Revert "Bla"
    This reverts commit 77bf6e9953b807e0050f55985b093cf3201280de.

--- protected/config/main.php
+++ protected/config/main.php
@@ -32,17 +32,10 @@ return array(
 
 	// application components
 	'components'=>array(
-		//Yii node socket register
-		'nodeSocket' => array(
-		    'class' => 'application.extensions.yii-node-socket.lib.php.NodeSocket',
-		    'host' => 'localhost',  // default is 127.0.0.1, can be ip or domain name, without http
-		    'port' => 3001      // default is 3001, should be integer
-		)		
-
 		'image'=>array( 
 			'class'=>'application.extensions.image.CImageComponent',
 			'driver'=>'GD',
-		),
+),
 
 		'user'=>array(
 			// enable cookie-based authentication
@@ -98,5 +91,10 @@ return array(
 		'adminEmail'=>'ando@ut.ee',
 	),
 
-
+	//Yii node socket register
+	'nodeSocket' => array(
+    'class' => 'application.extensions.yii-node-socket.lib.php.NodeSocket',
+    'host' => 'localhost',  // default is 127.0.0.1, can be ip or domain name, without http
+    'port' => 3001      // default is 3001, should be integer
+	)
 );
