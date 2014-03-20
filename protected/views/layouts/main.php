c56f927eb3f6cac99fcf7c652855aff3eab1cc60
Ando <ando@ladaklubi.ee>
2014-03-20 23:08:12 +0200

    Viewport fix - optimizing

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- 
+    <meta name=viewport content="width=device-width, initial-scale=1">
     <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame. Remove this if you use the .htaccess -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
  
