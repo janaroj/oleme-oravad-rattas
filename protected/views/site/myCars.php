b0b32fd451515a9cacba18e619e0b3aa942a31df
mikski <maasik2@gmail.com>
2014-03-31 22:54:01 +0300

    myCars.php struktureerimine

--- protected/views/site/myCars.php
+++ protected/views/site/myCars.php
@@ -1,40 +1,30 @@
 
 <div class="container">
-
-    <div class="content">
-
-    	<?php include 'admin-sidebar.php'; ?>
-    
-    	<div class="content-small">
-    	        <h1>Siin saad sa oma autosid muuta</h1>
-    	        <table class="request-table">
-                <thead>
-                    <th>Auto</th>
-                    <th>Väljalaskeaasta</th>
-                    <th>Asukoht</th>
-                    <th>Staatus</th>
-                    <th></th>
-                    <th></th>
-                </thead>
-                 <?php foreach ($cars as $car) { ?>
-         		<tr>
-          			<td><?php echo $car['make'];echo " ";echo $car['model'] ?></td>
-          			<td><?php echo $car['year']; ?></td>
-          			<td><?php echo $car['location']; ?></td>
-          			<td><?php echo $car['status']; ?></td>
-                <td class="req-answer"><?php echo CHtml::link('Muuda',array('changeCar','carId' =>$car['ID']))?></td>
-                <td class="req-answer"><?php echo CHtml::link('Kustuta',array('deleteCar','carId' =>$car['ID']))?></td>          			
-          			
-          		</tr>
-          <?php } ?>
-             
-                   
-                    </td>
-                </tr>
-            </table>
-
-    	</div>
-   <div class="clear" />
-
-   </div>
+  <div class="content">
+  	<?php include 'admin-sidebar.php'; ?>    
+    <div class="content-small">
+    	<h1>Siin saad sa oma autosid muuta</h1>
+    	<table class="request-table">
+        <thead>
+          <th>Auto</th>
+          <th>Väljalaskeaasta</th>
+          <th>Asukoht</th>
+          <th>Staatus</th>
+          <th></th>
+          <th></th>
+        </thead>
+        <?php foreach ($cars as $car) { ?>
+        <tr>
+    			<td><?php echo $car['make'];echo " ";echo $car['model'] ?></td>
+    			<td><?php echo $car['year']; ?></td>
+    			<td><?php echo $car['location']; ?></td>
+    			<td><?php echo $car['status']; ?></td>
+          <td class="req-answer"><?php echo CHtml::link('Muuda',array('changeCar','carId' =>$car['ID']))?></td>
+          <td class="req-answer"><?php echo CHtml::link('Kustuta',array('deleteCar','carId' =>$car['ID']))?></td>
+  		  </tr>
+        <?php } ?>
+      </table>
+    </div>
+    <div class="clear" />
+  </div>
 </div>
