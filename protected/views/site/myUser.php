b3f4657d60b3da4b1a6e2cb5c13ae457e39a8cd0
mikski <maasik2@gmail.com>
2014-03-31 23:02:10 +0300

    myUser.php struktureerimine

--- protected/views/site/myUser.php
+++ protected/views/site/myUser.php
@@ -1,24 +1,20 @@
 
 <div class="container">
-	<div class="content">
-		
+	<div class="content">		
 		<?php include 'admin-sidebar.php'; ?>
-
 		<div class="content-small">
 		    <h1><?php echo "Tere ";echo Yii::app()->user->getName();?></h1> 
 			<p>Siin on sinu kasutaja leht</p>
-			<p>Sul on 
-			
-				<?php
-				$userId = Yii::app()->user->id;
-				$sql="SELECT count(*) as total from users inner join cars on users.id = cars.userId where users.id = $userId";
-				$connection=Yii::app()->db; 
-				$command=$connection->createCommand($sql);
-				$result=$command->query();
-				foreach ($result as $value) {
-					echo $value['total'];
-				}?> autot
-
+			<p>Sul on			
+				<?php   $userId = Yii::app()->user->id;
+						$sql="SELECT count(*) as total from users inner join cars on users.id = cars.userId where users.id = $userId";
+						$connection=Yii::app()->db; 
+						$command=$connection->createCommand($sql);
+						$result=$command->query();
+						foreach ($result as $value) {
+							echo $value['total'];
+						}?>
+				autot
 			</p>
 		</div>
 		<div class="clear"></div>
