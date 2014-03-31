8291458ea09035696cd5ba76f898eee7f08d8aaf
mikski <maasik2@gmail.com>
2014-03-31 23:10:18 +0300

    registration.php struktureerimine

--- protected/views/site/registration.php
+++ protected/views/site/registration.php
@@ -1,48 +1,37 @@
 <div class="container">
     <div class="content">
-        <h2>Registreeru</h2>    
-        
+        <h2>Registreeru</h2>            
         <div class="form">
-        <?php echo CHtml::beginForm(); ?>
-         
-            <?php echo CHtml::errorSummary($model); ?>
-         
+            <?php   echo CHtml::beginForm(); 
+                    echo CHtml::errorSummary($model); ?>         
             <div class="row">
-                <?php echo CHtml::activeLabel($model,'Email'); ?>
-                <?php echo CHtml::activeTextField($model,'email') ?>
-            </div>
-         
+                <?php echo CHtml::activeLabel($model,'Email'); 
+                      echo CHtml::activeTextField($model,'email') ?>
+            </div>         
             <div class="row">
-                <?php echo CHtml::activeLabel($model,'Parool'); ?>
-                <?php echo CHtml::activePasswordField($model,'password') ?>
+                <?php echo CHtml::activeLabel($model,'Parool'); 
+                      echo CHtml::activePasswordField($model,'password') ?>
             </div>
-
              <div class="row">
-                <?php echo CHtml::activeLabel($model,'Parool uuesti'); ?>
-                <?php echo CHtml::activePasswordField($model,'confirmedPassword') ?>
+                <?php echo CHtml::activeLabel($model,'Parool uuesti'); 
+                      echo CHtml::activePasswordField($model,'confirmedPassword') ?>
             </div>
-         
-
             <div class="row">
-            <?php echo CHtml::activeLabel($model,'Eesnimi'); ?>
-                <?php echo CHtml::activeTextField($model,'firstName') ?>
+                <?php echo CHtml::activeLabel($model,'Eesnimi'); 
+                      echo CHtml::activeTextField($model,'firstName') ?>
             </div>
-
             <div class="row">
-            <?php echo CHtml::activeLabel($model,'Perekonnanimi'); ?>
-                <?php echo CHtml::activeTextField($model,'lastName') ?>
+                <?php echo CHtml::activeLabel($model,'Perekonnanimi'); 
+                      echo CHtml::activeTextField($model,'lastName') ?>
             </div>
-
             <div class="row">
-                <?php echo CHtml::activeLabel($model,'Telefon'); ?>
-                <?php echo CHtml::activeTextField($model,'phone') ?>
+                <?php echo CHtml::activeLabel($model,'Telefon'); 
+                      echo CHtml::activeTextField($model,'phone') ?>
             </div>
-         
             <div class="row submit">
                 <?php echo CHtml::submitButton('Registreeri'); ?>
-            </div>
-         
-        <?php echo CHtml::endForm(); ?>
+            </div>         
+            <?php echo CHtml::endForm(); ?>
         </div><!-- form -->
         <div class="clear"></div>
     </div>
