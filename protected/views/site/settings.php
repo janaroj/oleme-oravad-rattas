a1a2d462dc14ef8f8ff418fe6d816ca9cc2f1517
mikski <maasik2@gmail.com>
2014-03-31 23:12:02 +0300

    settings.php struktureerimine

--- protected/views/site/settings.php
+++ protected/views/site/settings.php
@@ -1,46 +1,34 @@
+<div class="container">
+    <div class="content">        
+        <?php include 'admin-sidebar.php' ?>                
+        <div class="content-small">
+            <h1>Siin saad sa oma seadeid muuta</h1>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Eesnimi'); 
+                      echo CHtml::activeTextField($model,'firstName') ?>
+            </div>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Perekonnanimi'); 
+                      echo CHtml::activeTextField($model,'lastName') ?>
+            </div>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Telefon'); 
+                      echo CHtml::activeTextField($model,'phone') ?>
+            </div>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Parool'); 
+                      echo CHtml::activePasswordField($model,'password') ?>
+            </div>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Uus Parool'); 
+                      echo CHtml::activePasswordField($model,'password') ?>
+            </div>
+            <div class="row">
+                <?php echo CHtml::activeLabel($model,'Korda parooli'); 
+                      echo CHtml::activePasswordField($model,'password') ?>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
 
-
-	<div class="container">
-	
-	    <div class="content">
-	        
-	        <?php include 'admin-sidebar.php' ?>
-	                
-	        <div class="content-small">
-	            <h1>Siin saad sa oma seadeid muuta</h1>
-	               <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Eesnimi'); ?>
-                    <?php echo CHtml::activeTextField($model,'firstName') ?>
-                </div>
-
-                 <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Perekonnanimi'); ?>
-                    <?php echo CHtml::activeTextField($model,'lastName') ?>
-                </div>
-
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Telefon'); ?>
-                    <?php echo CHtml::activeTextField($model,'phone') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Parool'); ?>
-                    <?php echo CHtml::activePasswordField($model,'password') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Uus Parool'); ?>
-                    <?php echo CHtml::activePasswordField($model,'password') ?>
-                </div>
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Korda parooli'); ?>
-                    <?php echo CHtml::activePasswordField($model,'password') ?>
-                </div>
-
-	        </div>
-	        <div class="clear"></div>
-	    </div>
-	</div>
-	
-	
