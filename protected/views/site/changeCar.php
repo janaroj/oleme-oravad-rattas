01c6ce20bbc585932c039dbae81410df1f8179c7
mikski <maasik2@gmail.com>
2014-03-31 22:30:06 +0300

    changeCar.php struktureerimine

--- protected/views/site/changeCar.php
+++ protected/views/site/changeCar.php
@@ -1,89 +1,65 @@
-	<div class="container">
-	
-	    <div class="content">
-	        
-            <?php include 'admin-sidebar.php'; ?>
-
-            <div class="content-small">
-	            <h1>Siin saad sa muuta oma autot</h1>
-
-           <?php echo CHtml::form('','post',array('enctype'=>'multipart/form-data')); ?>
-            	<div class="form">
-            	<?php echo CHtml::beginForm(); ?>
-             
-                <?php echo CHtml::errorSummary($model); ?>
-             
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Mark'); ?>
-                    <?php echo CHtml::activeTextField($model,'make') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Mudel'); ?>
-                    <?php echo CHtml::activeTextField($model,'model') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Asukoht'); ?>
-                    <?php echo CHtml::activeTextField($model,'location') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Väljalaskeaasta'); ?>
-                    <?php echo CHtml::activeTextField($model,'year') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Värvus'); ?>
-                    <?php echo CHtml::activeTextField($model,'color') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Saadavus'); ?>
-                    <?php echo CHtml::activeTextField($model,'status') ?>
-                </div>
-
-                <div class="row description">
-                    <?php echo CHtml::activeLabel($model,'Kirjeldus'); ?>
-                    <?php echo CHtml::activeTextArea($model,'description') ?>
-                </div>
-
-                <div class="row">
-                    <?php echo CHtml::activeLabel($model,'Kilomeetri hind'); ?>
-                    <?php echo CHtml::activeTextField($model,'price') ?>
-                </div>
-
-                <div class="row">
-                   <?php echo CHtml::activeLabel($model,'Põhipilt'); ?>
-                    <?php echo CHtml::activeFileField($model, 'image'); ?>
-                 
-               </div>
-
-                <div class="row">
-                   <?php echo CHtml::activeLabel($model,'Ülejäänud pildid'); ?>
-                   <?php 
-                $this->widget('CMultiFileUpload', array(
-                'name' => 'images',
-                'htmlOptions' => array('enctype' => 'multipart/form-data','multiple' => 'multiple'),
-                'accept' => 'jpeg|jpg|gif|png', // useful for verifying files
-                'duplicate' => 'Duplicate file!', // useful, i think
-                'denied' => 'Invalid file type', // useful, i think
-            ));
-                    ?>
-
-                 
-               </div>
-             
-               
-                <div class="row submit">
-                    <?php echo CHtml::submitButton('Muuda autot'); ?>
-                </div>
-             
-                <?php echo CHtml::endForm(); ?>
-                </div><!-- form -->
+<div class="container">	
+    <div class="content">	        
+        <?php include 'admin-sidebar.php'; ?>
+        <div class="content-small">
+            <h1>Siin saad sa muuta oma autot</h1>
+            <?php echo CHtml::form('','post',array('enctype'=>'multipart/form-data')); ?>
+        	<div class="form">
+        	    <?php   echo CHtml::beginForm();
+                        echo CHtml::errorSummary($model); ?>             
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Mark'); 
+                        echo CHtml::activeTextField($model,'make') ?>
             </div>
-	       <div class="clear"></div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Mudel'); 
+                        echo CHtml::activeTextField($model,'model') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Asukoht'); 
+                        echo CHtml::activeTextField($model,'location') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Väljalaskeaasta'); 
+                        echo CHtml::activeTextField($model,'year') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Värvus'); 
+                        echo CHtml::activeTextField($model,'color') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Saadavus'); 
+                        echo CHtml::activeTextField($model,'status') ?>
+            </div>
+            <div class="row description">
+                <?php   echo CHtml::activeLabel($model,'Kirjeldus'); 
+                        echo CHtml::activeTextArea($model,'description') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Kilomeetri hind'); 
+                        echo CHtml::activeTextField($model,'price') ?>
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Põhipilt'); 
+                        echo CHtml::activeFileField($model, 'image'); ?>                 
+            </div>
+            <div class="row">
+                <?php   echo CHtml::activeLabel($model,'Ülejäänud pildid'); 
+                        $this->widget('CMultiFileUpload', array(
+                        'name' => 'images',
+                        'htmlOptions' => array('enctype' => 'multipart/form-data','multiple' => 'multiple'),
+                        'accept' => 'jpeg|jpg|gif|png', // useful for verifying files
+                        'duplicate' => 'Duplicate file!', // useful, i think
+                        'denied' => 'Invalid file type', // useful, i think
+                        )); ?>                 
+            </div>
+            <div class="row submit">
+                <?php echo CHtml::submitButton('Muuda autot'); ?>
+            </div>
+            <?php echo CHtml::endForm(); ?>
+            </div><!-- form -->
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
 
-	    </div>
-	</div>
-	
