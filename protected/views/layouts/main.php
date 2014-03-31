096631d7ced08419688219f55a9cc097e56ab624
Ando <ando@ladaklubi.ee>
2014-03-31 20:58:22 +0300

    Optimizing

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,7 +15,7 @@
     <?php   
         $cs = Yii::app()->getClientScript();
         $cs->registerCssFile('css/style.css');
-        $cs->registerScriptFile('//code.jquery.com/jquery-1.10.2.min.js',null,array=('async'=>'async'))));
+        $cs->registerScriptFile('//code.jquery.com/jquery-1.10.2.min.js',null,array('async'=>'async'))));
         $cs->registerScriptFile('js/custom.js');?>
  
 </head>
