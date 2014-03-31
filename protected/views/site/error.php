bc6bae70d60722d11d1cbe1df6aa9f19c4a9d121
Ando <ando@ladaklubi.ee>
2014-03-31 22:35:36 +0300

    Error 404

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,15 +1,16 @@
 <?php
 /* @var $this SiteController */
 /* @var $error array */
-
 $this->pageTitle=Yii::app()->name . ' - Error';
 $this->breadcrumbs=array(
-	'Error',
-);
+	'Error',);
 ?>
-
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
+<div class="container">
+	<div class="content">
+		<h2>Error <?php echo $code; ?></h2>
+		<div class="error">
+		<?php echo CHtml::encode($message); ?>
+		</div>
+	</div>
+	<div class="clear"></div>
 </div>
