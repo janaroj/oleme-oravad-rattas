2f5690e4d3708d68c9ae733e498d543975d93ef5
Janar Ojalaid <janarojalaid@gmail.com>
2014-04-16 19:07:57 +0300

    commit count + 1

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 
-<<!DOCTYPE html>
 <html>
-<head>
-  <title></title>
-</head>
-<body>
-
-</body>
-</html>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
