43d065c5ce241ff93c295d9622f40b1ea52253dc
Janar Ojalaid <janarojalaid@gmail.com>
2014-04-25 01:21:57 +0300

    customlinkpager

---
+++ protected/extensions/CustomLinkPager.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * CLinkPager class file.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.yiiframework.com/
+ * @copyright 2008-2013 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+/**
+ * CLinkPager displays a list of hyperlinks that lead to different pages of target.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @package system.web.widgets.pagers
+ * @since 1.0
+ */
+class CustomLinkPager extends CLinkPager
+{
+	
+	/**
+	 * Creates the page buttons.
+	 * @return array a list of page buttons (in HTML code).
+	 */
+	protected function createPageButtons()
+	{
+		if(($pageCount=$this->getPageCount())<=1)
+			return array();
+
+		list($beginPage,$endPage)=$this->getPageRange();
+		$currentPage=$this->getCurrentPage(false); // currentPage is calculated in getPageRange()
+		$buttons=array();
+
+
+		// internal pages
+		for($i=$beginPage;$i<=$endPage;++$i)
+			$buttons[]=$this->createPageButton($i+1,$i,$this->internalPageCssClass,false,$i==$currentPage);
+
+	
+		return $buttons;
+	}
+
+}
